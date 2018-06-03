6967b0f451c1476ac1df30c97f49c88e93ccef8c
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2018-06-03 15:25:21 +0200

    Clean up smooth

--- outfiles/demo.py
+++ outfiles/demo.py
@@ -4,6 +4,7 @@ import click, re, os, numpy as np
 import matplotlib.pyplot as plt
 from numpy import exp
 import itertools as it
+import pdb
 
 from experiment import Experiment, get_experiment_series
 
@@ -14,21 +15,31 @@ def cli():
 @cli.command()
 @click.argument('folder', type=click.Path())
 def all(folder):
-    '''Show all data sets in one folder. Shortcut for 'demo.py show file1 -e
-    file2 ....' '''
+    """
+    Show all data sets in one folder. Shortcut for 'demo.py show file1 -e file2 ....'
+    """
     handles = []
     experiments = get_experiment_series(folder)
     for ex in experiments:
-        handles.append(ex.plot(plt.plot, label=ex.height)[0])
+        handles.append(
+            plt.plot(
+                ex.distance,
+                ex.weight,
+                label=ex.height)[0]
+            )
     plt.legend()
     plt.show()
 
 @cli.command()
-@click.option('--infile', '-i', type=click.Path(), multiple=True)
-@click.option('--zero/--no-zero', '-z/-nz', help='Set the true zero to be the \
-average of the last centimeter', default=False)
+@click.option('--infile', '-i',
+        type=click.Path(), multiple=True)
+@click.option('--zero/--no-zero', '-z/-nz',
+    help='Set the true zero to be the average of the last centimeter',
+    default=False)
 def show(infile, zero=False):
-    """Render data in provided input file using row 2 and 3."""
+    """
+    Render data in provided input file using row 2 and 3.
+    """
     assert len(infile) > 0, "At least one inputfile must be provided"
     experiments = []
     for f in infile:
@@ -39,59 +50,50 @@ def show(infile, zero=False):
     handles = []
     for e in experiments:
         if zero:
-            e.trueZero()
-        handles.append(e.plot(plt.plot, e.height)[0])
+            handles.append(
+                plt.plot(e.distance, e.zeroed_weight)
+                )
+        else:
+            handles.append(plt.plot(
+                e.distance, e.weight, label=e.height)[0])
     plt.legend(handles=handles)
     plt.show()
 
 @cli.command()
 @click.argument('folder', type=click.Path())
 def max(folder):
-    '''Find max value of every file in the provided folder.'''
+    """
+    Find max value of every file in the provided folder.
+    """
     experiments = get_experiment_series(folder)
     experiments = sorted(experiments, key=lambda x: x.height)
     plt.plot([ex.height for ex in experiments], [ex.maxWeight() for ex in experiments])
     plt.show()
 
+def smooth_values(distance, weight, fwhm=3):
+    def fwhm2sigma(fwhm):
+        return fwhm / np.sqrt(8 * np.log(2))
+
+    sigma = fwhm2sigma(fwhm)
+    smoothed_vals = np.zeros(weight.shape)
+    for i, d in enumerate(distance):
+        kernel = np.exp(-(distance - d) ** 2 / (2 * sigma ** 2))
+        kernel = kernel / sum(kernel)
+        smoothed_vals[i] = sum(weight * kernel)
+    return smoothed_vals
+
 @cli.command()
 @click.argument('infile', type=click.Path())
 @click.option('--dx', default=0.1)
 @click.option('--minval', default=1, type=int)
-def smooth(infile, dx=0.1, minval=1):
-    a = np.loadtxt(inf, skiprows=2, delimiter=',')
-    new_weight = smooth_values(a[:, 1], a[:, 2])
-    smooth_gradient = np.gradient(new_weight, dx)
-
-    handleold, = plt.plot(a[:, 1], a[:, 2], label='old')
-    handlenew, = plt.plot(a[:, 1], new_weight, label='new')
-    hgradold, = plt.plot(a[:, 1], np.gradient(a[:, 2], dx), label='gradient of data')
-    hgradnew, = plt.plot(a[:, 1], np.gradient(new_weight, dx), label='gradient of smooth')
-
-    # Find where the graph hits about zero
-    breaks_at = 0
-    weight_at_break = 0
-    offset = 200
-    right = 0
-    for i, grad_of_weigth in enumerate(smooth_gradient[offset:]):
-        if abs(grad_of_weigth) < minval:
-            breaks_at = i
-            right = a[offset+i, 1]
-            weight_at_break = a[i+offset, 2]
-            break
-    plt.plot([a[offset+i, 1], a[offset+i, 1]], [-50, 50], 'red')
-
-    print(weight_at_break)
-    # Find where that zero is mirrored
-    left = 0
-    for i, val1, val2 in zip(it.count(), a[:,2], a[1:, 2]):
-        if abs(val1) < abs(weight_at_break) and abs(weight_at_break) <= abs(val2):
-            left = a[i, 1]
-            plt.plot([a[i, 1], a[i, 1]], [-50, 50], 'red')
-            break
-
-    click.echo('Found difference {}'.format(right - left))
-    plt.legend([handlenew, handleold])
-    plt.show()
+@click.option('--plot/--no-plot', default=False)
+@click.option('--fwmh', default=3)
+def smooth(infile, dx, minval, plot, fwmh):
+    """
+    Find the breaking point of the provided graph
+    """
+    ex = Experiment(infile)
+    ex.find_breaks(minval=minval, dx=dx, plot=plot)
 
 if __name__=='__main__':
     cli()
--- outfiles/experiment.py
+++ outfiles/experiment.py
@@ -1,11 +1,13 @@
 import numpy as np
-import os, re
+import os, re, itertools as it
+from matplotlib import pyplot as plt
 
 def get_experiment_series(folder):
     experiments = []
     for roots, dirs, files in os.walk(folder):
         for f in files:
-            experiments.append(Experiment(os.path.join(folder, f)))
+            experiments.append(
+                    Experiment(os.path.join(folder, f)))
     return experiments
 
 class Experiment:
@@ -20,28 +22,52 @@ class Experiment:
         a = np.loadtxt(inf, skiprows=2, delimiter=',')
         self.distance = a[:, 1]
         self.weight = a[:, 2]
-
-    def plot(self, plotfunc, label=None):
-        return plotfunc(self.distance, self.weight, label=label)
+        self.trueZero()
+        self.smooth()
 
     def smooth(self, fwhm=3):
         def fwhm2sigma(fwhm):
             return fwhm / np.sqrt(8 * np.log(2))
 
         sigma = fwhm2sigma(fwhm)
-        smoothed_force = np.zeros(self.force.shape)
+        smoothed_vals = np.zeros(self.weight.shape)
         for i, d in enumerate(self.distance):
             kernel = np.exp(-(self.distance - d) ** 2 / (2 * sigma ** 2))
             kernel = kernel / sum(kernel)
-            smoothed_vals[i] = sum(self.force * kernel)
-        return distance, smoothed_force
+            smoothed_vals[i] = sum(self.weight * kernel)
+        self.smoothed = smoothed_vals
+
+    def find_breaks(self, fwmh=3, dx=0.1, offset=200, minval=1, plot=False):
+        smooth_gradient = np.gradient(self.smoothed, dx)
+        weight_at_break = breaks_at = left = right = 0
+        for i, weight_gradient in enumerate(smooth_gradient[offset:]):
+            if abs(weight_gradient) < minval:
+                breaks_at = i+offset
+                weight_at_break = self.weight[breaks_at]
+                right = self.distance[breaks_at]
+                break
+
+
+        for i, val1, val2 in zip(it.count(), self.weight, self.weight[1:]):
+            if abs(val1) < abs(weight_at_break) and abs(weight_at_break) < abs(val2):
+                left = self.distance[i]
+                break
+
+        if plot:
+            plt.plot([left]*2, [-50, 50], 'red')
+            plt.plot([right]*2, [-50, 50], 'red')
+            plt.plot([left-5, right+5], [weight_at_break]*2)
+            plt.plot(self.distance, self.weight)
+            plt.show()
+
+        return right - left
 
     def trueZero(self):
         trueZero = []
-        for i, val in enumerate(a[:, 2]):
-            if i>500:
+        for i, val in enumerate(self.weight):
+            if i > 500:
                 trueZero.append(val)
-        self.weight = self.weight[:, 2] - np.avg(trueZero)
+        self.zeroed_weight = self.weight - np.average(trueZero)
 
     def maxWeight(self):
         return  np.min(self.weight)
